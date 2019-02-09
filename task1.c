6f1db46c6d4ca3fe8cba4edb438a5bfdf389d166
Tom1tk <39281660+Tom1tk@users.noreply.github.com>
2019-02-09 17:51:50 +0000

    Add files via upload
    PC upload

---
+++ task1.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+int main() // This function will return an integer
+{
+    char nameTeam1[20]; //Declares variable "nameTeam1" which has 20 characters
+    char nameTeam2[20]; //Declares variable "nameTeam2"  which also has 20 characters
+    int team1Score, team2Score; //Declares two integer variables "team1Score" and "team2Score"
+    printf("Please enter the name of team one: "); //Prints a statement for the user, requesting them to enter the desired name of the first team
+    scanf("%s", nameTeam1); //This line takes the input from the user and assigns it to the variable "nameTeam1"
+    printf("Please enter the name of team two: "); //Repeats the same process for "nameTeam2" to request and assign the name of the second team
+    scanf("%s", nameTeam2);
+    printf("Please enter the score for %s: ", nameTeam1); //Requests the score for the first named team
+    scanf("%d", & team1Score); //Assigns the user input to variable "team1Score"
+    printf("Please enter the score for %s: ", nameTeam2); //Repeats the process for "team2Score"
+    scanf("%d", & team2Score);
+    if (team1Score > team2Score) //If statement which checks if the integer value for "team1Score" is higher than "team2Score"
+    {
+        printf("%s scores 3 points and %s scores 0 points", nameTeam1, nameTeam2 ); //If the statement is true, since the first team has a higher score, print that they score 3 points and the second team scores 0
+    }
+    else //If the statement is not true, carrys on to the next if statement
+      if (team1Score < team2Score) //Compares the two vales to see if the second team's score value is higher than the team's
+        {
+            printf("%s scores 3 points and %s scores 0 points", nameTeam2, nameTeam1 ); //Print that the second team get 3 points and the first team gets 0
+        }
+        else //If neither statements are met then both teams must have the same score
+    {
+            printf("Both %s and %s score 1 point", nameTeam1, nameTeam2); //Since it must be a draw, both teams recieve 1 point
+    }
+    return 0; //The function returns integer 0
+}
---
+++ task2.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+
+int main() //Starts a function that will return an integer
+{
+    char name[20], response, grade; //Initialises 3 string variables
+    int mark; //Initialises the marks integer variable
+    do {  //Starts a do while loop
+      printf("Enter a student name: "); //Asks the user to input the student name
+      scanf("%s", name); //reads the input from the user and assigns the pointer to the variable "name" as a string
+      printf("Enter the student’s mark: "); //Asks the user to input the student's mark
+      scanf("%d", &mark); //reads the input from the user and assigns the pointer to the variable "mark" as an integer
+      printf("%s", name); //Outputs the name of the student from the variable "name"
+      printf(" recieves the grade: "); //Outputs a string to form a sentence containing the student's name and grade
+      if (mark < 40){ //Checks if the value of "mark" is less than 40
+        printf("F"); //If so, the outputted grade is an F
+      } //closes the if loop
+      else if (mark <= 49){ //If the previous statement isn't true, checks to see if "mark" is less than or equal to 49
+          printf("D"); //If so, the outputted grade is a D
+        } //closes the if loop
+          else if (mark <= 59){ //If the previous statement also isn't true, checks to see if "mark" is less than or equal to 59
+            printf("C"); //If so, the outputted grade is a C
+          } //closes the if loop
+            else if (mark <= 69){ //If the previous statement also isn't true, checks to see if "mark" is less than or equal to 69
+              printf("B"); //If so, the outputted grade is a B
+            } //closes the if loop
+            else { //If all the previous statements are false then the grade must be higher than 69 so another if statement is not necessary
+              printf("A"); //If so, the outputted grade must be an A
+            } //closes the if loop
+      getchar(); //reads a single character from stdin
+      printf("\nDo you want to add another student? Type Y for yes and N for No\n");
+      scanf("%c", &response);
+    } while (response == 'Y');
+    return 0;
+}
+
+/*
+Assignment task 2
+
+The program above reads from the keyboard names and marks of students. The
+user is asked
+after each student if they want to continue by entering another student or
+exit the program.
+a.   Comment the existing code.
+b.   Write the code that displays for each student the grade achieved.
+c.   Extend the program to count how many grades of A, B, C, D and F were
+achieved.
+Marking scheme:
+1 mark for presenting the program in week 4
+Up to 5 marks for items a. and b.
+Up to 4 marks for item c.
+You need to do items a. and b. before approaching item c.
+*/

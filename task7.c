26520da4fa654ba7a9445523dc69c2d066092256
tom1-laptop <tomtomp446446@gmail.com>
2019-03-28 13:06:59 +0000

    task 7 done

--- task7.c
+++ task7.c
@@ -1,12 +1,42 @@
 #include <stdio.h>
 
-int main ()
+int smallest(int num1, int num2)
 {
-
-
-
-return 0
+  if(num1 > num2){
+    return num2;
+  }else
+  return num1;
 }
+
+int main()
+{
+  int arr[10], pos[10], i, x, j=0, small;
+
+  printf("How many numbers?:");
+  scanf(" %d", &x);
+  printf("\nInput %d numbers:\n", x);
+  for (i = 0; i < x; i++){
+     scanf(" %d", &arr[i]);
+   }
+
+  for (i = 0; i < x; i++){
+     if(arr[i]>0){
+      pos[j] = arr[i];
+      j++;
+    }
+   }
+
+   small = pos[0];
+   for (i = 1; i < j; i++){
+      small = smallest(small, pos[i]);
+    }
+
+  printf("The smallest positive number is: %d", small);
+
+return 0;
+}
+
+
 
 /*
 Assignment task 7
